3654a0cfe07d9aaa1dc0b610d9d3a43e501e6dd5
destrynielsen-maker <destrynielsen@gmail.com>
2026-08-25 14:55:42 -0600

    Register Beaverton permit collector

--- src/oregon_permits/collectors/__init__.py
+++ src/oregon_permits/collectors/__init__.py
@@ -1,7 +1,8 @@
+from .beaverton import BeavertonCollector
 from .bend import BendCollector
 from .eugene import EugeneCollector
 from .gresham import GreshamCollector
 from .hillsboro import HillsboroCollector
 from .portland import PortlandCollector
 
-COLLECTORS = [PortlandCollector(), EugeneCollector(), BendCollector(), HillsboroCollector(), GreshamCollector()]
+COLLECTORS = [PortlandCollector(), EugeneCollector(), BendCollector(), HillsboroCollector(), GreshamCollector(), BeavertonCollector()]
